874582bd37fd6613adfd6fb589524edf1e30a959
Ben Godfrey <bcgodfrey91@gmail.com>
2016-12-04 00:36:52 -0700

    First Commit

---
+++ app/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600,
+    show: false
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
